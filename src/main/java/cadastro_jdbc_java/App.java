8583f845cd67a81d1bc0779fe8e1db2f6a58376c
ViniciusRicciChamps <vcricci26@hotmail.com>
2022-07-28 18:04:39 -0300

    criado a classe de conexao no postgresql

---
+++ src/main/java/cadastro_jdbc_java/App.java
@@ -0,0 +1,12 @@
+package cadastro_jdbc_java;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello word");
+	}
+
+}
---
+++ src/main/java/conexao_jdbc/SingleConnection.java
@@ -0,0 +1,42 @@
+package conexao_jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+	
+	private static String url = "jdbc:postgresql://localhost:5432";
+	private static String password = "1234";
+	private static String user = "postgres";
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+
+
+
+	private static void conectar() {
+		try {
+			
+			if(connection == null) {
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false);
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static Connection getConnection() {
+		
+		return connection;
+	}
+}
